58db4fca5d92e93322f961f350f1273f53f7530d
DmitryKhiliuk <khiliukbrest@gmail.com>
2022-04-16 18:25:02 +0300

    13 controlled input, checkbox, select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from "react";
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -27,5 +28,23 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
 
     return <><input ref={inputRef}/> <button onClick={save}>save</button> - actual value: {value}</>
 }
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    return <input value={parentValue} onChange={(e) => {setParentValue(e.currentTarget.value)}}/>
+}
+export const Controlled = () => {
+    const [parentValue, setParentValue] = useState(true)
+    return <input type={'checkbox'} checked={parentValue} onChange={(e) => {setParentValue(e.currentTarget.checked)}}/>
+}
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('1')
+    return <select value={parentValue} onChange={(e) => setParentValue(e.currentTarget.value)}>
+        <option value="1">Minsk</option>
+        <option value="2">Warshaw</option>
+        <option value="3">Kyiv</option>
+    </select>
+}
+
 export const ControlledInputWithFixedValue = () => <input value={'it-incubator.by'}/>
 
